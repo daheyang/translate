aeee2189afbfa26131c6700c4e0e3b9ffd9b3d01
Dahe <subwayang00@gmail.com>
2021-12-18 03:38:40 +0900

    entity

--- app/src/main/java/org/daheyang/translate/MainActivity.kt
+++ app/src/main/java/org/daheyang/translate/MainActivity.kt
@@ -2,10 +2,30 @@ package org.daheyang.translate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import org.daheyang.translate.model.PapagoEntity
+import org.daheyang.translate.model.PapagoServiceCreator
+import retrofit2.Call
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val papagoService = PapagoServiceCreator().create()
+        val call = papagoService.requestTranslation()
+        call.enqueue(object : retrofit2.Callback<PapagoEntity> {
+            override fun onResponse(call: Call<PapagoEntity>, response: Response<PapagoEntity>) {
+                val result = response.body()
+
+                Log.d("요청 성공!", "${result?.message}")
+            }
+
+            override fun onFailure(call: Call<PapagoEntity>, t: Throwable) {
+                Log.d("요청 실패", "onFailure!")
+                t.printStackTrace()
+            }
+        })
     }
 }
--- app/src/main/java/org/daheyang/translate/model/PapagoEntity.kt
+++ app/src/main/java/org/daheyang/translate/model/PapagoEntity.kt
@@ -1,4 +1,13 @@
 package org.daheyang.translate.model
 
 class PapagoEntity {
+    var message: ResultMessage? = null
+
+    inner class  ResultMessage {
+        var result: Result? = null
+
+        inner class Result {
+            var translatedText: String? = null
+        }
+    }
 }
