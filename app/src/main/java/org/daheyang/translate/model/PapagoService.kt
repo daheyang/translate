b601c1d81dd73d650409e8dc1453ffa13654b6ea
Dahe <subwayang00@gmail.com>
2021-12-19 21:06:45 +0900

    translate Kor into Eng

--- app/src/main/java/org/daheyang/translate/model/PapagoService.kt
+++ app/src/main/java/org/daheyang/translate/model/PapagoService.kt
@@ -1,12 +1,12 @@
 package org.daheyang.translate.model
 
+import org.daheyang.translate.sourceText
 import retrofit2.Call
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.Header
 import retrofit2.http.POST
 
-val sourceText = "this is android. Today is December 19th. It's cold today."
 
 interface PapagoService {
     @FormUrlEncoded
@@ -15,6 +15,6 @@ interface PapagoService {
                            @Header("X-Naver-Client-Secret")apiKey: String = Configs.apiKey,
                            @Field("source")fromLang: String = "en",
                            @Field("target")toLang: String = "ko",
-                           @Field("text")text: String? = sourceText) : Call<PapagoEntity>
+                           @Field("text")text: String? = sourceText ) : Call<PapagoEntity>
 
 }
